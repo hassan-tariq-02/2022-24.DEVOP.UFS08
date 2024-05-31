afc7f2aa2a43afb12999d2cad342d2f905d681c0
hassan-tariq-02 <130441243+hassan-tariq-02@users.noreply.github.com>
2024-05-31 14:57:37 +0000

    a-simple-function added mainTF added provider

---
+++ a-simple-function/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+variable "GOOGLE_CLOUD_PROJECT_ID" {
+  type = string
+}
+provider "google" {
+  project = var.GOOGLE_CLOUD_PROJECT_ID
+}
